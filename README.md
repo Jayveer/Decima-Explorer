80cd3dd364c06a15723a7afbddb2146e683d781c
Jayveer <jayveerk@gmail.com>
2020-07-19 23:50:26 +0100

    Added WIP decryption support

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 # Decima Explorer
 
 
-Decima Explorer is a free and open-source program designed to allow you to unpack files from the archive structure used by games using the Decima engine. Currently files must be extracted using their ID as I am unaware of how the file names are mapped. I suspect a hashing algorithm is used for lookup but am unsure of which one.
+Decima Explorer is a free and open-source program designed to allow you to unpack files from the archive structure used by games using the Decima engine. Currently files must be extracted using their ID as I am unaware of how the file names are mapped.
+
+Early work-in-progress support for encrypted files has been added thanks to Ekey, and [Wunkolo](https://github.com/Wunkolo) for researching, implementing and documenting the decryption algorithm. Wunkolo's [implementation can be found here](https://github.com/Wunkolo). 
 
 This program uses [Ooz](https://github.com/powzix/ooz) created by [Powzix](https://github.com/powzix) for decompression. It is slightly modified for this programs purposes. Ooz is under GPL licensing.
+
+This program also uses [Murmur3](https://github.com/PeterScott/murmur3) by [Peter Scott](https://github.com/PeterScott/murmur3).
 
 If anyone has any issue with this project's existence please feel free to reach out to me.
 
 ### To Do
  - Create GUI variant
  - Implement Repack command
- - Understand how the file strings match to files
+ - Understand how the file strings match to files (Appears to be Murmur3Hash but not seeing expected results)
 
 ##  Usage
 
--- archive/DecimaArchive.h
+++ archive/DecimaArchive.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "../ooz/Kraken.h"
+#include "../hash/md5.h"
+#include "../hash/MurmurHash3.h"
+#include <string>
 #include <inttypes.h>
 #include <vector>
 
 typedef struct DecimaHeader {
 	uint32_t magic; //0x20304050
-	uint32_t version; //0x3D
+	uint32_t key;
 	uint64_t fileSize;
 	uint64_t dataSize;
 	uint64_t fileTableCount;
@@ -16,20 +19,20 @@ typedef struct DecimaHeader {
 
 typedef struct DecimaFileEntry {
 	uint32_t entryNum;
-	uint32_t unknown;
-	uint64_t unknown2; //maybe file hash, not sure
+	uint32_t key;
+	uint64_t hash;
 	uint64_t offset;
 	uint32_t size;
-	uint32_t unknown3;
+	uint32_t key2;
 } DecimaFileTable;
 
 typedef struct DecimaChunkEntry {
 	uint64_t uncompressedOffset; //relative offset once uncompressed
 	uint32_t uncompressedSize;
-	uint32_t unknown;
+	uint32_t key;
 	uint64_t compressedOffset;
 	uint32_t compressedSize;
-	uint32_t unknown2;
+	uint32_t key2;
 } DecimaChunkTable;
 
 typedef std::vector<uint8_t> DataBuffer;
@@ -42,18 +45,29 @@ private:
 
 	std::string filename;
 
+	uint8_t seed = 0x2A;
+	uint32_t murmurSalt[4]  = { 0x0FA3A9443, 0x0F41CAB62, 0x0F376811C, 0x0D2A89E3E };
+	uint32_t murmurSalt2[4] = { 0x06C084A37, 0x07E159D95, 0x03D5AF7E8, 0x018AA7D3F };
+
 	void parseHeader(FILE* file);
 	void parseFileTable(FILE* f, uint64_t fileTableCount);
 	void parseChunkTable(FILE* f, uint64_t chunkTableCount);
 
 	bool checkMagic();
+	bool isEncrypted();
+	void decryptHeader();
+	void decryptFileTable();
+	void decryptChunkTable();
 	uint32_t getFileEntryIndex(int id);
 	DecimaFileEntry getFileEntry(int id);
 	void setFilename(std::string filename);
 	int findChunkWithOffset(uint64_t offset);
 	DataBuffer extract(DecimaFileEntry fileEntry);
+	void decryptChunkData(int32_t id, DataBuffer* data);
 	DataBuffer getChunkData(DecimaChunkEntry chunkEntry);
 	int calculateChunkTableOffset(uint64_t fileTableCount);
+	void cipher(uint32_t key, uint32_t key2, uint32_t* src);
+	void dataCipher(uint32_t chunkID, uint8_t* src, int size);
 	int writeDataToFile(DataBuffer data, std::string filename);
 	uint64_t calculateFirstContainingChunk(uint64_t fileOffset, int chunkSize);
 	uint64_t calculateLastContainingChunk(uint64_t fileOffset, int fileSize, int chunkSize);
